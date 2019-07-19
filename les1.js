acce6885f757b4b92397a7214e525502cf76631a
ymk <ymk@inbox.ru>
2019-07-19 13:49:44 +0300

    Human Employee

--- les1.js
+++ les1.js
@@ -36,3 +36,102 @@ ob = calculateArea('Square', 5, 5);
 console.log( ob );
 
 
+/* 3. Необходимо написать иерархию классов вида:
+Human -> Employee -> Developer
+Human -> Employee -> Manager
+*/
+
+// У класса Human должны быть следующие параметры: 
+// name (строка), age (число), dateOfBirth (строка или дата) 
+class Human {
+    constructor(name, age, dateOfBirth) {
+        this.name = name;
+        this.age = age;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    // геттер
+    get humanName() {
+        return this.name;
+    }
+
+    // сеттер
+    set humanName(name) {
+        this.name = name;
+    }
+
+    // вычисляемое название метода
+    ['printName']() {
+        console.log(this.name);
+    }
+
+    // В классе Human должен присутствовать метод displayInfo, который возвращает строку со всеми параметрами экземпляра Human.
+    displayInfo() {
+        return `${this.name} ${this.age} ${this.dateOfBirth}`;
+    }
+}
+  
+const human = new Human ('John', 20, 1999);
+console.log(human.humanName); // "John"
+console.log(human); // "John"
+
+human.humanName = 'Max';
+human.printName(); // "Max"
+
+console.log( ' displayInfo() = ' , human.displayInfo() );
+
+// наследуем класс Human
+// У класса Employee должны присутствовать параметры: salary (число), department (строка).
+class Employee extends Human {
+    constructor(name, age, dateOfBirth, salary, department) {
+      super(name, age, dateOfBirth); // вызывает конструктор базового класса
+      this.salary = salary;
+      this.department = department;
+    }
+  
+    printName() {
+      console.log(this.department, this.salary);
+    }
+  
+    printAll() {
+      this.printName();
+      super.printName(); // вызывает метод базового класса
+    }
+
+    // В классе Employee должен быть реализовать такой же метод (displayInfo), 
+    // который вызывает базовый метод и дополняет его параметрами из экземпляра Employee.
+    displayInfo() {
+        return `${super.displayInfo()} ${this.department} ${this.salary}`;
+    }
+}
+  
+const employee = new Employee('John',20, 1999, 10000, 'IT');
+employee.printAll(); // IT 10000, John
+
+console.log( employee.displayInfo() );
+
+// Каждый Менеджер (Manager) должен иметь внутренний массив своих сотрудников (разработчиков), а также методы по удалению/добавлению разработчиков.
+class Manager {
+    constructor(managerID) {
+        this.managerID = managerID;
+    }
+  
+    printManagerID() {
+        console.log(this.managerID);
+    }
+}
+
+// Каждый Разработчик (Developer) должны иметь ссылку на Менеджера и методы для изменения менеджера (имеется в виду возможность назначить другого менеджера).
+class Developer {
+    constructor(developerID, managerID) {
+        this.managerID = managerID;
+        this.developerID = developerID;
+    }
+  
+    printManagerID() {
+        console.log(this.managerID);
+    }
+    printDeveloperID() {
+        console.log(this.developerID);
+    }
+}
