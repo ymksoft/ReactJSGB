040aa73ed34bfd4e8b9598406f304dd24c887193
ymk <ymk@inbox.ru>
2019-08-02 13:52:44 +0300

    layouts wrapper changes

--- src/app/layouts/Layout.js
+++ src/app/layouts/Layout.js
@@ -15,30 +15,32 @@ export default class Layout extends React.Component {
   render() {
     return (
       <>
-        <Menu brand={this.brand}>
-          <MenuItem href="/" active={this.isActive('/')}>
-            Главная
-          </MenuItem>
-          <MenuItem href="/posts" active={this.isActive('/posts')}>
-            Посты
-          </MenuItem>
-          <MenuItem href="/comments" active={this.isActive('/comments')}>
-            Комментарии
-          </MenuItem>
-          <MenuItem href="/users" active={this.isActive('/users')}>
-            Пользователи
-          </MenuItem>
-        </Menu>
-        <div className="container">
+        <div className="wrapper container">
+          <header></header>
+          <Menu brand={this.brand}>
+            <MenuItem href="/" active={this.isActive('/')}>
+              Главная
+            </MenuItem>
+            <MenuItem href="/posts" active={this.isActive('/posts')}>
+              Посты
+            </MenuItem>
+            <MenuItem href="/comments" active={this.isActive('/comments')}>
+              Комментарии
+            </MenuItem>
+            <MenuItem href="/users" active={this.isActive('/users')}>
+              Пользователи
+            </MenuItem>
+          </Menu>
+          <div class="heading"></div>
           <div className="row">
             <div className="col-12">
               {this.props.children}
             </div>
           </div>
+          <footer className="card-footer">
+            &copy; 2019
+          </footer>
         </div>
-        <footer className="card-footer">
-          &copy; 2019
-        </footer>
       </>
     );
   }
